37b2168d5d94bfed0d18dd2f5ef930aa8e69e2fd
wb <wpjtek@yahoo.com>
2014-12-30 13:22:02 +0100

    Add license and keywords to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 try:
     from setuptools import setup
 except ImportError:
@@ -5,12 +11,14 @@ except ImportError:
 
 config = {
     'description': 'Zalando CLI that works on Zalando API',
+    'license': 'Apache 2',
+    'long_description': read('README.rst')
     'author': 'Wojciech Barczynski',
     'url': 'URL to get it at.',
     'download_url': '',
     'author_email': 'wpjtek@yahoo.com',
     'version': '0.1',
-    'install_requires': ['requests', 'argparse', 'simplejson'],
+    'install_requires': read('requirements.txt'),
     'scripts': [],
     'packages': ['shiny_client'],
     'name': 'shiny_client',
@@ -18,6 +26,7 @@ config = {
         'console_scripts': [
             'zshiny = shiny_client.client:main'
         ]
-    }
+    },
+    'keywords': "zalando API REST commandline cli"
 }
 setup(**config)
